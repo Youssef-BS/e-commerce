3d13b67d1fc69bab9118b7b7ab9a87435c86e82f
Ben said Youssef <youssefbensaid827@gmail.com>
2024-09-18 15:05:08 +0100

    add navbar and footer and home componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent],
+  declarations: [AppComponent, AuthComponent, HeaderComponent , HomeComponent , FooterComponent],
   imports: [BrowserModule, FormsModule], 
   providers: [],
   bootstrap: [AppComponent]
